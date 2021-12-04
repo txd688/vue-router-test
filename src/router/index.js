f6e69bd3082a918f1711e278dc5dfab8fc0e301d
txd <63951240@qq.com>
2021-12-04 21:30:39 +0800

    vue-router 源码 效果简易实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import VueRouter from "../myRouter";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
@@ -22,8 +22,8 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+  // mode: "history",
+  // base: process.env.BASE_URL,
   routes,
 });
 
